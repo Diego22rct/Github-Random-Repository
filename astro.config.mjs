8123fb8252d1a8d7a31d394e4989a84505b3a302
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-10-01 16:37:48 -0500

    Remove unused authentication integration from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import auth from 'auth-astro';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth()]
 });
